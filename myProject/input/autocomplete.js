f08d682fc9c513ca4b6cdda11e32de8c701343f0
tunok <32317831+tunok@users.noreply.github.com>
2018-09-04 10:52:23 +0300

    Add files via upload

---
+++ myProject/input/autocomplete.js
@@ -0,0 +1,35 @@
+;(function autocomplete() {
+//Массив строк.
+    var listArr = [
+        'Toyota',
+        'Opel',
+        'BMW',
+        'Pontiac',
+        'Subaru',
+        'Renault',
+        'Audi'];
+//Переменные - создание, атрибуты и добавление
+    var divTag = document.createElement('div'),
+        inputTag = document.createElement('input'),
+        datalistTag = document.createElement('datalist');
+
+    inputTag.setAttribute('list', 'namesList');
+    inputTag.setAttribute('placeholder', 'Click for car choice ...');
+    datalistTag.setAttribute('id', 'namesList');
+
+    document.body.appendChild(divTag);
+    divTag.appendChild(inputTag);
+    divTag.appendChild(datalistTag);
+//Функция и цикл заполнения вариантов выбора
+    function createChoiceOption(n) {
+        var optionTag = document.createElement('option');
+        optionTag.innerText = listArr[n];
+        datalistTag.appendChild(optionTag);
+    }
+    for (let i = 0; i < listArr.length; i++) {
+        createChoiceOption(i);
+    }
+    inputTag.addEventListener('focus', function () {
+        datalistTag.style = 'display: inherit';
+    })
+}) ();
